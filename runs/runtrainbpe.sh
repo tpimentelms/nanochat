fc3314706863e43b22f9f7e43b627a13c3d490c0
Marius Tomek <marius.tomek@asvz.ch>
2026-04-20 22:51:08 +0200

    undid changes to trainbpe

--- runs/runtrainbpe.sh
+++ runs/runtrainbpe.sh
@@ -3,7 +3,7 @@
 # Running a full training run of BPE on the CPU (sufficient RAM recommended)
 
 # Run as:
-# bash runs/runbpe_cpu.sh
+# bash runs/runtrainbpe.sh
 
 # NOTE: Training LLMs requires GPU compute and $$$. You will not get far on your Macbook.
 # Think of this run as educational/fun demo, not something you should expect to work well.
@@ -24,10 +24,16 @@ MAX_CHARS="${MAX_CHARS:-10000000000}"
 
 # train tokenizer on ~2B characters (~34 seconds on my MacBook Pro M3 Max)
 python -m nanochat.dataset -n 50
+python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=262144 --tokenizer-name=256k
+
 python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=131072 --tokenizer-name=128k
 
 python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=65536 --tokenizer-name=64k
 
 python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=32768 --tokenizer-name=32k
 
-python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=131072 --tokenizer-name=128k
+python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=16384 --tokenizer-name=16k
+
+python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=8192 --tokenizer-name=8k
+
+python -m scripts.tok_train --max-chars=$MAX_CHARS --tokenizer-type=BPE --vocab-size=4096 --tokenizer-name=4k
