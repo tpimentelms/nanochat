d784a1bbb02f05134cf163bac362d3c08a451658
Marius Tomek <marius.tomek@avsz.ch>
2026-04-20 21:22:26 +0200

    trained BPE models

--- runs/runtrainbne.sh
+++ runs/runtrainbne.sh
@@ -10,7 +10,7 @@
 # You may also want to run this script manually and one by one, copy pasting commands into your terminal.
 
 # all the setup stuff
-export NANOCHAT_BASE_DIR="$HOME/nanochat/data/BNE_unrest"
+export NANOCHAT_BASE_DIR="$HOME/nanochat/data/BNE"
 mkdir -p $NANOCHAT_BASE_DIR
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
@@ -22,16 +22,16 @@ fi
 
 # train tokenizer on ~2B characters (~34 seconds on my MacBook Pro M3 Max)
 python -m nanochat.dataset -n 8
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=262144 --tokenizer-name=tokenizer_256k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=262144 --tokenizer-name=256k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=131072 --tokenizer-name=tokenizer_128k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=131072 --tokenizer-name=128k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=65536 --tokenizer-name=tokenizer_64k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=65536 --tokenizer-name=64k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=32768 --tokenizer-name=tokenizer_32k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=32768 --tokenizer-name=32k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=16384 --tokenizer-name=tokenizer_16k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=16384 --tokenizer-name=16k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=8192 --tokenizer-name=tokenizer_8k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=8192 --tokenizer-name=8k
 
-python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=4096 --tokenizer-name=tokenizer_4k
+python -m scripts.tok_train --max-chars=2000000000 --tokenizer-type=BNE --vocab-size=4096 --tokenizer-name=4k
